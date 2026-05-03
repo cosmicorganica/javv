645e955d01b029052deaf918c0025d533aeaf930
cosmicorganica <dksivasharma@gmail.com>
2026-05-03 14:59:15 +0530

    Delete app/src directory

--- app/src/main/java/com/example/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.repository;
-
-import com.example.user.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
